e729392df3ab211321730c610ed0317c0836c536
Shane Liesegang <shane@techie.net>
2013-01-16 15:35:45 -0500

    Well, the least we can do is use it to prompt for player input.

--- angel_samples_win.bat
+++ angel_samples_win.bat
@@ -3,15 +3,15 @@ REM Because MS can't get their shit together for reasonable
 REM command line builds without setting all kinds of environment
 REM variables.
 
-if [%1]==[] goto usage
+if [%1]==[] (
+	set /p VERS=Version number?: 
+) else (
+	set VERS=%1
+)
 
 set OLDDIR=%CD%
 cd C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin
 CALL vcvars32.bat
 chdir /d %OLDDIR%
-python angel_samples_win.py %1
-goto :eof
+python angel_samples_win.py %VERS%
 
-:usage
-@echo Needs a version number.
-exit /B 1
