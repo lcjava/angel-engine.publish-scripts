7b616d76a5dae91369489454763e739c11351833
Shane Liesegang <shane@techie.net>
2013-01-17 13:46:01 -0500

    Fixing tabs.

--- angel_release.py
+++ angel_release.py
@@ -6,8 +6,8 @@ import os
 scripts = ["angel_pack", "angel_doc", "angel_samples_mac"]
 
 if len(sys.argv) < 2:
-  print "Pass me a version number, yo."
-  exit(1)
+    print "Pass me a version number, yo."
+    exit(1)
 
 start_dir = os.getcwd()
 for script in scripts:
