e735f771b2cbf89e4d672b9a0db41ff7aecc86e4
Shane Liesegang <shane@techie.net>
2013-02-01 01:34:21 -0500

    Yay lightbox? Or something. Kinda buggy. Need better images.

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
 
+    // $('.angel-lightbox > .thumbnail').click(
+    //     function (event) {
+    //         var targetID = event.target.id;
+    //         var index = parseInt(event.target.id.substring("angel-lightbox-".length));
+    //         if (!isNaN(index)) {
+    //             $('.carousel').carousel(index-1);
+    //         }
+    //     }
+    // );
+
+    $('.carousel').carousel();
+});
